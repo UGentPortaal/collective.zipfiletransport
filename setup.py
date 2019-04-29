bd77a1f84929b70e001b7c08e5c350a5b475e4e6
Dimitri Boone <Dimitri.Boone@ugent.be>
2019-04-29 14:35:05 +0200

    Back to development: 3.2.6-ugentPCM941

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ __version__   = '$ Revision 0.0 $'[11:-2]
 from setuptools import setup, find_packages
 import os
 
-version = '3.2.5-ugentPCM940'
+version = '3.2.6-ugentPCM941.dev0'
 
 setup(name='collective.zipfiletransport',
       version=version,
